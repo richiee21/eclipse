76af296c5103d6f00997d1b853d129ec6f7b9a29
richiee21 <richiee21@live.com>
2016-07-20 12:07:32 +0530

    this is my first commit

---
+++ NewProject/src/GIT.java
@@ -0,0 +1,9 @@
+
+public class GIT {
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+System.out.println("Hello, World");
+System.out.println("this is shafeeque");
+
+}
+} 
