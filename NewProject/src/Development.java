901738b2faf51ce8af12cb2d6fef4185fc91fb52
richiee21 <richiee21@live.com>
2016-07-20 12:45:28 +0530

    second branch

---
+++ NewProject/src/Development.java
@@ -0,0 +1,9 @@
+
+public class Development {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World");
+		System.out.println("this is Development");
+
+		}
+		} 
